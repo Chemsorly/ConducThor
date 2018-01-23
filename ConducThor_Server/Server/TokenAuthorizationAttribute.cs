ba3331dd3cb970852f62f94507f545bbfe73bfd2
Chemsorly <contact@chemsorly.com>
2018-01-23 19:54:06 +0100

    fix token based sec

--- ConducThor_Server/Server/TokenAuthorizationAttribute.cs
+++ ConducThor_Server/Server/TokenAuthorizationAttribute.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace ConducThor_Server.Server
 {
-    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
     public class TokenAuthorizationAttribute : AuthorizeAttribute
     {
+        private readonly string AuthToken = $"ThisIsACustomTokenToPreventSpamBecauseFuckIt";
+
         public override bool AuthorizeHubConnection(Microsoft.AspNet.SignalR.Hubs.HubDescriptor hubDescriptor, IRequest request)
         {
             string token = request.Headers["authtoken"];
-            if (token == $"ThisIsACustomTokenToPreventSpamBecauseFuckIt")
+            if (token == AuthToken)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext hubIncomingInvokerContext, bool appliesToMethod)
+        {
+            string token = hubIncomingInvokerContext.Hub.Context.Headers["authtoken"];
+            if (token == AuthToken)
             {
                 return true;
             }
